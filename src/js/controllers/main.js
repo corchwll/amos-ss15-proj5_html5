01f5f045e091cff17c761c8d02165bf94271a2e4
Ulrich Mackert <Ulrich.Mackert@gmx.de>
2015-06-02 20:33:05 +0200

    User Stories #63 & #62: All projects, except default projects, ordered alphabetically in project list

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -12,9 +12,22 @@ angular.module('MobileTimeRecording.controllers.Main', ['MobileTimeRecording.ser
   	});
   };
 
+   $scope.orderProjects = function(project) {
+    if(project.id === '00001') {
+      return -1;
+    } else if(project.id === '00002') {
+      return -1;
+    } else if(project.id === '00003') {
+      return -1;
+    } else if(project.id === '00004') {
+      return -1;
+    } 
+    return project.name;
+  };
+
   $scope.viewProject = function(projectId) {
     $location.path('/viewProject/' + projectId);
-  }
+  };
 
   $scope.addProject = function() {
   	$location.path('/addProject');
@@ -28,22 +41,22 @@ angular.module('MobileTimeRecording.controllers.Main', ['MobileTimeRecording.ser
       modal.element.modal();
       modal.close.then(function(result) {
         if(result === 'Yes') {
-          deleteProject(project.id)
+          deleteProject(project.id);
         } else {        
           $scope.updateProjects();
         }
-      })
+      });
     });
-  }
+  };
   var deleteProject = function(projectId) {
     Projects.archive(projectId).then(function() {
       $scope.updateProjects();
     });
-  }
+  };
 })
 
 .controller('ModalController', function($scope, close) {
   $scope.close = function(result) {
     close(result);
-  }
+  };
 });
