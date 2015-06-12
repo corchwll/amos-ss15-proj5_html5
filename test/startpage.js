ec9c52e672cca98824b138ab192aebd7a4ca52c6
Ulrich Mackert <Ulrich.Mackert@gmx.de>
2015-06-12 14:34:01 +0200

    Added dummy-test for controller viewProject. Test always succeeds, if test-setup is correct. (-> No testing of functionality, but of test-setup)

---
+++ test/startpage.js
@@ -0,0 +1,14 @@
+describe('Controller viewProject', function() {
+	beforeEach(module('MobileTimeRecording.controllers.ViewProject'));
+
+	beforeEach(inject(function(_$controller_){
+		$controller = _$controller_;
+	}));
+
+	describe('Dummy', function() {
+		it('DummyScope', function() {
+			expect(true).toBe(true);
+			console.log("Dummy test executed");
+		});	
+	});
+});
