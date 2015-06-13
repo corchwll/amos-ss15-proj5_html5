fa9aff96206b5d07ea9ad3ea127ed7dc8ccc3bcc
Ulrich Mackert <Ulrich.Mackert@gmx.de>
2015-06-13 19:31:02 +0200

    Added functionality to load individual controller within test file in order to enable unit tests.

--- test/startpage.js
+++ test/startpage.js
@@ -1,14 +1,61 @@
-describe('Controller viewProject', function() {
-	beforeEach(module('MobileTimeRecording.controllers.ViewProject'));
+describe('ControllerviewProject', function() {
 
+
+	/*beforeEach(function() {
+	    module(angular.module.name);
+
+	    module(function($provide) {
+	      $provide.service('MobileTimeRecording.services.Database', myServiceName);
+	    });
+
+	    inject(function($injector) {
+	      $controller = $injector.get('MobileTimeRecording.controllers.ViewProject');
+	    });
+	 });*/
+	//beforeEach(module('ngNotifyProvider.ngNotify'));
+	beforeEach(module('MobileTimeRecording.controllers.ViewProject'));
+	//beforeEach(module('MobileTimeRecording.controllers.ViewProject', ['MobileTimeRecording.services.Database']));
+	var $controller;
 	beforeEach(inject(function(_$controller_){
 		$controller = _$controller_;
 	}));
 
+	/*var ViewProjectController, $rootScope;
+	beforeEach(inject(function($injector) {
+		ViewProjectController = $injector.get('ViewProjectController');
+		$rootScope = $injector.get('$rootScope');
+	}));*/
+/*
 	describe('Dummy', function() {
 		it('DummyScope', function() {
 			expect(true).toBe(true);
-			console.log("Dummy test executed");
+			//console.log("Dummy test executed");
 		});	
 	});
+*/
+
+	describe('Function getProject', function() {
+		var $scope, controller;
+
+		beforeEach(function() {
+			$scope = {};
+			controller = $controller('ViewProjectController', { $scope: $scope });
+		});
+
+
+		it('DummyScope', function() {
+			expect(true).toBe(true);
+			//console.log("Dummy test executed");
+		});	
+
+		/*
+		it('Get project with id == 1', function() {
+			//var $scope = {};
+			//var controller = $controller('ViewProjectController', { $scope: $scope });
+			//$scope.getById(1);
+			//expect($scope.project).toEqual('Holiday');
+		});
+*/
+	});
+
 });
