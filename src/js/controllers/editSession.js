f6aca28e95dba034e5e4f529873663bbe98d4fbc
Ulrich Mackert <Ulrich.Mackert@gmx.de>
2015-06-16 11:13:25 +0200

    Added annotation for controller EditSessionController

--- src/js/controllers/editSession.js
+++ src/js/controllers/editSession.js
@@ -2,6 +2,11 @@ angular.module('MobileTimeRecording.controllers.EditSession', ['MobileTimeRecord
 
 .controller('EditSessionController', function($scope, Sessions, Projects, $location, ngNotify, $timeout, $routeParams){
 
+	/**
+	 * This function adds a new session into the database and generates a respective notification.
+	 * 
+	 * @param  session An object containing data of a session
+	 */
 	var addSession = function(session) {
 	  Sessions.add(session).then(function() {
 	  	ngNotify.set(project.name + ' successfully added', {
