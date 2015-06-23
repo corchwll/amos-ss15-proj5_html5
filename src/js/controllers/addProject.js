3ac8c3a45fe6d1e3d61ebbca461a48bc412a1e3e
Ulrich Mackert <Ulrich.Mackert@gmx.de>
2015-06-23 15:21:27 +0200

    Added functionality to track button by implementing function trackProject and the related result and error functions. Additionally removed the restriction to use the track button only once every field of the form is filled correctly.

--- src/js/controllers/addProject.js
+++ src/js/controllers/addProject.js
@@ -1,6 +1,13 @@
 angular.module('MobileTimeRecording.controllers.AddProject', ['MobileTimeRecording.services.Database'])
 
 .controller('AddProjectController', function($scope, Projects, $location, ngNotify, $timeout){
+	/**
+	 * This variable is used to restrict the navigator.geolocation localization to using GPS only
+	 * 
+	 */
+	var trackOptions = {
+	  enableHighAccuracy: true
+	};
 	
 	/**
 	 * This function adds a new project into the database, gives a respective notification and forwards to the main screen.
@@ -21,5 +28,50 @@ angular.module('MobileTimeRecording.controllers.AddProject', ['MobileTimeRecordi
 	  		$location.path('#/');
 	  	}, 4000);
 	  });
+  };
+
+  /**
+   * This function starts a GPS localization in order to get the longitude and latitude of the current position.
+   * Note that by W3C standard the user has to accept the usage of this functionality explicitly.
+   * 
+   */
+  $scope.trackProject = function() {
+  	console.log("track button pressed");
+	if (navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(pushPosition, trackError, trackOptions);
+    } else { 
+        ngNotify.set('Geolocation is not supported by this browser', {
+			type: 'error',
+			position: 'top',
+			duration: 3000
+		});
+    }
+  };
+
+  /**
+   * This function is the result function of function trackProject. It pushes the values of the longitude and latitude into the respective form fields.
+   * 
+   * @param  position The position object created and forwarded by the getCurrentPosition function of the geolocation API
+   */
+  pushPosition = function(position) {
+  	console.log("push position:");
+  	console.log(position.coords.longitude);
+  	console.log(position.coords.latitude);
+  	for($i = 0; $i < 2; $i++) {
+	  	$scope.project.longitude = position.coords.longitude;
+	  	$scope.project.latitude = position.coords.latitude;
+	 }
+  };
+
+  /**
+   * This function is the error function of function trackProject. It shows a notification that the localization via GPS was unsuccessful.
+   * 
+   */
+  trackError = function() {
+	ngNotify.set('GPS localization unsuccessful', {
+		type: 'error',
+		position: 'top',
+		duration: 3000
+	});
   };
 });
