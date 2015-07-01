55ebfdd7c04f14376c2fc89ddf10a3817da21b32
Ulrich Mackert <Ulrich.Mackert@gmx.de>
2015-07-01 12:23:47 +0200

    Updated test for controller viewProject

--- test/viewProjectTests.js
+++ test/viewProjectTests.js
@@ -5,19 +5,49 @@ describe('Controller: ViewProjectController', function () {
 	// load the controller's module
 	beforeEach(module('MobileTimeAccounting.controllers.ViewProject'));
 
+	// load the database module
+	beforeEach(module('MobileTimeAccounting.services.Database'));
+
 	var ViewProjectController,
 	scope;
 
 	// Initialize the controller and a mock scope
-	beforeEach(inject(function ($controller, $rootScope) {
+	beforeEach(inject(function ($controller, $rootScope, DB, Projects) {
 		scope = $rootScope.$new();
 		ViewProjectController = $controller('ViewProjectController', {
 			$scope: scope,
-			$routeParams: {projectId: '12345'}
+			$routeParams: {projectId: '02976'}
 		});
+		DB.init();
+		Projects.populate();
 	}));
 
-	it('should attach a list of awesomeThings to the scope', function () {
-		expect(true).toBe(true);
+	describe('Function getProject', function () {
+		/*
+		it('PRETEST', function () {
+			$routeParams =  {projectId: '00001'};
+			expect($routeParams.projectId).toBe('00001');
+		});
+		*/
+		/*beforeEach(function(done) {
+			scope.getProject().then(function() {
+				done();
+			});
+		});*/
+	
+		it('Get correct predefined poject Vacation', function (done) {
+			$routeParams =  {projectId: '00001'};
+			console.log("BEGIN");
+			inject(function() {
+				console.log("Call me Injector");
+				scope.getProject()
+				done();
+			});
+			expect(scope.title).not.toBeNull();
+			console.log(scope.title);
+			console.log("Finite");
+			expect(scope.title).toBe("Vacation");
+		});
 	});
+	
 });
