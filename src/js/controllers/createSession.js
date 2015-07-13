1b573c5f8bb07a68bcb0ce65423e6bea4b0db055
Ulrich Mackert <Ulrich.Mackert@gmx.de>
2015-07-13 17:52:36 +0200

    Removed usage of cordova plugin, which is only part of the Phonegap-version of the app.

--- src/js/controllers/createSession.js
+++ src/js/controllers/createSession.js
@@ -46,20 +46,15 @@ angular.module('MobileTimeAccounting.controllers.CreateSession', ['MobileTimeAcc
 							Sessions.checkFullOverlapping(session.timestamp_start, session.timestamp_stop).then(function(result) {
 								if(result.overlappings === 0) {
 									Sessions.add(session).then(function() {
-										if(day === moment().format("YYYY-MM-DD")) {
-											cordova.plugins.notification.local.cancel(1, function() {
-											  console.log("Daily notification canceled for today");
-											});
-										}
-								  	ngNotify.set('Session successfully added', {
-							  			type: 'success',
-							  			position: 'top',
-							  			duration: 3000
-							  		});
-								  	$timeout(function() {
-								  		$(location).attr('href', '#/viewProject/' + session.project_id);
-								  	}, 4000);
-							  	});
+									  	ngNotify.set('Session successfully added', {
+								  			type: 'success',
+								  			position: 'top',
+								  			duration: 3000
+								  		});
+									  	$timeout(function() {
+									  		$(location).attr('href', '#/viewProject/' + session.project_id);
+									  	}, 4000);
+								  	});
 								} else {
 									ngNotify.set('You have already recorded for this time', {
 										type: 'error',
